c2eb79ca2d134ab0f42b1689c6d41af5d96862f1
Michel <106674127+MichelZero@users.noreply.github.com>
2023-05-22 16:20:26 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,18 @@
+
+#
+# autores:
+# Cristiano, Michel
+# 
+# Data: 22/05/2023
+# #
+# 
+# 1 – Imprima de 1 a 9. 
+###
+# entrada de dados
+valor = int(input("Digite um valor: "))
+
+# processamento
+for i in range(1, valor + 1):
+    print(i, end=" ")
+print()
+print("Fim do programa")
